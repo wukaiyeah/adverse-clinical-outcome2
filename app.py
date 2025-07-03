3a7b3152ae40a3343eb32d0ae9d911d342c3882b
wukaiyeah <scorpio1990@yeah.net>
2025-07-03 15:14:46 +0800

    debug

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ feature_names = [
 'Placental_abnormal_vasculature_diameter','Placental_abnormal_vasculature_area',
 'Intraplacental_dark_T2_band_area'
 ]
+# features = np.array([[40,274,7,1,1,0,4.3,1.21,2.3,2.783,4.0746]])
+# print(model.predict_proba(input)[0][1])
 
 # Streamlit user interface
 st.title("Adverse Clinical Outcome")
@@ -61,20 +63,16 @@ features = np.array([feature_values])
 
 if st.button("Predict"):
     # Predict class and probabilities
-    predicted_class = model.predict(features)[0]
-    predicted_proba = model.predict_proba(features)[0]
+    predicted_proba = model.predict_proba(input)[0][1]
 
     # Display prediction results
-    st.write(f"**Predicted Class:** {predicted_class}")
     st.write(f"**Prediction Probabilities:** {predicted_proba}")
 
-    # Generate advice based on prediction results
-    probability = predicted_proba[predicted_class] * 100
 
-    if predicted_class == 1:
+    if predicted_proba >= 0.5:
         advice = (
             f"According to our model, you have a high risk of heart disease. "
-            f"The model predicts that your probability of having heart disease is {probability:.1f}%. "
+            f"The model predicts that your probability of having heart disease is {predicted_proba:.1f}%. "
             "While this is just an estimate, it suggests that you may be at significant risk. "
             "I recommend that you consult a cardiologist as soon as possible for further evaluation and "
             "to ensure you receive an accurate diagnosis and necessary treatment."
@@ -82,7 +80,7 @@ if st.button("Predict"):
     else:
         advice = (
             f"According to our model, you have a low risk of heart disease. "
-            f"The model predicts that your probability of not having heart disease is {probability:.1f}%. "
+            f"The model predicts that your probability of not having heart disease is {predicted_proba:.1f}%. "
             "However, maintaining a healthy lifestyle is still very important. "
             "I recommend regular check-ups to monitor your heart health, "
             "and to seek medical advice promptly if you experience any symptoms."
@@ -92,9 +90,9 @@ if st.button("Predict"):
 
     # Calculate SHAP values and display force plot
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
+    shap_values = explainer.shap_values(pd.DataFrame(features, columns=feature_names))
 
-    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
+    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame(features, columns=feature_names), matplotlib=True)
     plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
 
     st.image("shap_force_plot.png")
